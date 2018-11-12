0a84e5e31f200c85dcd6532e5abe3ef66f21622b
Unknown <kacper.potyrala@gmail.com>
2018-11-12 11:36:09 +0100

    travis: Add travis system status check
    Closes https://github.com/ksdme/org-status/issues/2
    
    travis: Fix issues with code

--- org_status/status_providers/travis.py
+++ org_status/status_providers/travis.py
@@ -1,11 +1,15 @@
+import json
 import requests
 
 from org_status.status_providers import StatusProvider, Status
 
 
 class TravisBuildStatus(StatusProvider):
+    NAME = 'travis'
     BadgeTemplate = ('https://api.travis-ci.org/{group}'
                      '/{repo}.svg?branch={branch}')
+
+    TravisStatusUrl = 'https://pnpcptp8xh9k.statuspage.io/api/v2/status.json'
 
     def get_status(self, repo, host, branch='master'):
         badge_result = requests.get(self.get_badge_url(repo,
@@ -16,3 +20,14 @@ class TravisBuildStatus(StatusProvider):
             return Status.UNDETERMINED
 
         return self.status_from_badge_svg(badge_result.text)
+
+    @classmethod
+    def get_status_provider_status(cls):
+        try:
+            response = requests.get(cls.TravisStatusUrl)
+            status = json.loads(response.text)
+
+            description = status['status']['description']
+            return description.lower() == 'all systems operational'
+        except Exception:
+            return None
